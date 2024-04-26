1a547c262e850ec31e1592b8f54b7b1fd038383a
Luongtea072 <minhti195@gmail.com>
2024-04-27 04:40:14 +0700

    feat/ui: improve UI by adding some css properties and graph

--- UI/graph.py
+++ UI/graph.py
@@ -1,11 +1,17 @@
 import matplotlib.pyplot as plt
 import random
+from PIL import Image
+from PIL import ImageColor
+
 xLable = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 data1 = [220,220,220,220,220,220,220,220,220,220]
 
 data2 = data1
 
 data3 = data1
+
+ax = plt.axes()
+ax.set_facecolor("#00416A")
 
 for i in range(len(data1)):
     data1[i] += random.randrange(0, 10)*0.1
@@ -19,4 +25,7 @@ for i in range(len(data1)):
     data3[i] += random.randrange(0, 10)*0.1
 plt.plot(xLable, data3)
 plt.ylabel('Voltage')
+#hide x and y axis lable
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
 plt.show()
