558ee8435a2b3cbbfe3b0aadabb3c76de34bcd92
trungnt <nt.trungqb73@gmail.com>
2024-05-02 22:24:48 +0700

    ko bao gio len duoc

--- UI/test.py
+++ UI/test.py
@@ -1,55 +1,33 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.figure import Figure
-from collections import deque
-from itertools import count
-import matplotlib.pyplot as plt
-import random
-from matplotlib.animation import FuncAnimation
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel
+import requests
 
-class MyMainWindow(QMainWindow):
+class APICallExample(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Matplotlib Animation in PyQt")
 
-        # Tạo layout chính
-        layout = QVBoxLayout()
+        self.layout = QVBoxLayout()
+        self.label = QLabel("Data will be shown here.")
+        self.layout.addWidget(self.label)
 
-        # Tạo canvas Matplotlib
-        self.canvas = MplCanvas(self, width=5, height=4)
-        layout.addWidget(self.canvas)
+        self.button = QPushButton("Call API")
+        self.button.clicked.connect(self.call_api)
+        self.layout.addWidget(self.button)
 
-        # Tạo widget chính và thiết lập layout chính
-        main_widget = QWidget(self)
-        main_widget.setLayout(layout)
-        self.setCentralWidget(main_widget)
+        self.setLayout(self.layout)
 
-        # Tạo dữ liệu cho animation
-        self.xLable = deque(maxlen=30)
-        self.data1 = deque(maxlen=30)
-        self.index = count()
+    def call_api(self):
+        try:
+            response = requests.get(url='https://api.ipify.org/',headers='')
+            if response.status_code == 200:
+                data = response.json()
+                self.label.setText(str(data))
+    
+        except Exception as e:
+            self.label.setText("Error: " + str(e))
 
-        # Tạo animation
-        self.ani = FuncAnimation(self.canvas.figure, self.animate, interval=500)
-
-    def animate(self, i):
-        self.xLable.append(next(self.index))
-        self.data1.append(random.randrange(0, 9)*0.1 + 220)
-        self.canvas.ax.clear()
-        self.canvas.ax.plot(self.xLable, self.data1)
-        self.canvas.ax.set_xlabel('Time')
-        self.canvas.ax.set_ylabel('Voltage')
-        self.canvas.draw()
-
-class MplCanvas(FigureCanvas):
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
-        self.fig = Figure(figsize=(width, height), dpi=dpi)
-        self.ax = self.fig.add_subplot(111)
-        super().__init__(self.fig)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = MyMainWindow()
+    window = APICallExample()
     window.show()
     sys.exit(app.exec_())
